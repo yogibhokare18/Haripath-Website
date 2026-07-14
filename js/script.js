f2f3034c1cda9631de736c1d7d494900f92e33e2
Yoginand Bhokare <155873520+yogibhokare18@users.noreply.github.com>
2026-07-14 15:16:25 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
+/*function updateClock(){
 
+let now=new Date();
+
+document.getElementById("date").innerHTML=
+now.toLocaleDateString("mr-IN");
+
+document.getElementById("time").innerHTML=
+now.toLocaleTimeString("mr-IN");
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();*/
+
+alert("Script Loaded");
+
+function updateClock() {
+    const date = document.getElementById("date");
+    const time = document.getElementById("time");
+
+    if (date && time) {
+        let now = new Date();
+
+        date.innerHTML = now.toLocaleDateString("mr-IN");
+        time.innerHTML = now.toLocaleTimeString("mr-IN");
+    }
+}
+
+setInterval(updateClock, 1000);
+updateClock();
